bb94b4589007c5a9144652f3bbea581302f99538
Sokoana <Sokolova.sokoana@yandex.ru>
2024-09-07 18:36:09 +0300

    Final)))

--- src/test/java/AutorizationTest.java
+++ src/test/java/AutorizationTest.java
@@ -1,6 +1,8 @@
+import constans.UserDelete;
 import io.qameta.allure.junit4.DisplayName;
 import io.restassured.response.Response;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import constans.AutorizationUser;
@@ -11,14 +13,18 @@ public class AutorizationTest extends HttpPage {
     private String email;
     private String password;
     private UserData user;
+    private String name;
+    private String accessToken;
     @Before
     public void setUp() {
         baseURL();
         user = UserData.makeRandomUser();
-        BasicUser.buildUser(user);
+        Response response = BasicUser.buildUser(user);
         email = user.getEmail();
-         password = user.getPassword();
-    }
+        name = user.getName();
+        password = user.getPassword();
+        accessToken = response.jsonPath().getString("accessToken");
+            }
     @Test
     @DisplayName("Авторизация пользователя")
     public void testAutorizationUser() {
@@ -26,13 +32,17 @@ public class AutorizationTest extends HttpPage {
         Response response = BasicUser.autorizationUser(autorizationUser);
         response
                 .then().assertThat().statusCode(SC_OK).body("success", equalTo(true));
-            }
+           }
     @Test
     @DisplayName("Авторизация  с неверным логином и паролем")
     public void testLoginUserWrongData() {
         AutorizationUser autorizationUser = new AutorizationUser(RandomStringUtils.randomAlphabetic(8) + "@yandex.ru", RandomStringUtils.randomAlphabetic(5));
         Response response = BasicUser.autorizationUser(autorizationUser);
         response.then().assertThat().statusCode(SC_UNAUTHORIZED).body("success", equalTo(false));
+    }
+    @After
+    public void deleteUser() {
+   BasicUser.deleteUser(new UserDelete(email,name), accessToken).then().statusCode(SC_ACCEPTED);
     }
     }
 
